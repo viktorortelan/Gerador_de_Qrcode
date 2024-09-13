46411b53af3b0658e2ac87f2337a106ac8154860
viktorortelan <jvortelan@gmail.com>
2024-09-13 09:32:35 -0300

    fim

--- site/src/index.js
+++ site/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import App from './pages/home';
 import * as serviceWorker from './serviceWorker'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
